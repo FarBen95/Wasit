798ce274c3473abfa6a6f8f74b72249f20afe8af
flurt <azriantar@outlook.com>
2018-11-18 18:04:23 +0100

    camlecase userName and fixing password storage

--- api/models/user.js
+++ api/models/user.js
@@ -6,10 +6,10 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
   _id: { type: Schema.Types.ObjectId, required: true },
   name: { type: String, required: true },
-  username: { type: String, required: true },
+  userName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  hashedPassword: { type: String, required: true },
-  salt: { type: String, required: true },
+  password: { type: String, required: true },
+  salt: { type: String },
   imageUrl: { type: String },
   bio: { type: String },
   createdAt: { type: Date, default: Date.now },
