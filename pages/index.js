95489353ab324aa87a7fb2c732c30ceef4af7110
flurt <azriantar@outlook.com>
2018-12-25 17:46:06 +0100

    Setting up a simple nextjs front-end

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default () => (
+	<div>
+		<Head>
+			<title>Wasit</title>
+			<meta
+				name="viewport"
+				content="initial-scale=1.0, width=device-width"
+			/>
+		</Head>
+		<p>A storytelling platform for Arab writers !</p>
+	</div>
+);
