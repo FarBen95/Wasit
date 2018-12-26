ad1895fee9c11b47b525f0bbef2474bba4850491
flurt <azriantar@outlook.com>
2018-12-26 17:27:00 +0100

    Integrating nextjs with express server

--- api/app.js
+++ api/app.js
@@ -1,18 +1,21 @@
 require('dotenv').config();
 const express = require('express');
+const next = require('next');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const morgan = require('morgan');
 
-const app = express();
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-5pblm.mongodb.net/test?retryWrites=true`, { autoIndex: false, useNewUrlParser: true });
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(passport.initialize());
-app.use(morgan('dev'));
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USER}:${
+    process.env.DB_PASS
+  }@cluster0-5pblm.mongodb.net/test?retryWrites=true`,
+  { autoIndex: false, useNewUrlParser: true },
+);
 
 const port = process.env.PORT || 5000;
 
@@ -23,17 +26,28 @@ const authRoute = require('./routes/auth');
 require('./auth/google');
 require('./auth/jwt');
 
-app.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'Wasit server v1',
+app.prepare().then(() => {
+  const server = express();
+
+  server.use(bodyParser.urlencoded({ extended: true }));
+  server.use(bodyParser.json());
+  server.use(passport.initialize());
+  server.use(morgan('dev'));
+
+  server.get('*', (req, res) => handle(req, res));
+
+  server.get('/', (req, res) => {
+    res.status(200).json({
+      message: 'Wasit server v1',
+    });
   });
-});
 
-app.use('/publications', publicationRoute);
-app.use('/posts', postsRoute);
-app.use('/users', userRoute);
-app.use('/auth', authRoute);
+  server.use('/publications', publicationRoute);
+  server.use('/posts', postsRoute);
+  server.use('/users', userRoute);
+  server.use('/auth', authRoute);
 
-app.listen(port, () => {
-  console.log(`Wasit Server running on port ${port}`);
+  server.listen(port, () => {
+    console.log(`Wasit Server running on port ${port}`);
+  });
 });
